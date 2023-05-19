4294ac88851968ec2a7f480b138f6bf89e4ac61e
galargh <piotr.galar@gmail.com>
2023-05-19 09:06:23 +0200

    feat: implement github_issue_labels resource

---
+++ github/resource_github_issue_labels.go
@@ -0,0 +1,211 @@
+package github
+
+import (
+	"context"
+
+	"github.com/google/go-github/v52/github"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceGithubIssueLabels() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceGithubIssueLabelsCreate,
+		Read:   resourceGithubIssueLabelsRead,
+		Update: resourceGithubIssueLabelsUpdate,
+		Delete: resourceGithubIssueLabelsDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"repository": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				Description: "The GitHub repository.",
+			},
+			"label": {
+				Type:        schema.TypeSet,
+				Optional:    true,
+				Description: "List of labels",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "The name of the label.",
+						},
+						"color": {
+							Type:        schema.TypeString,
+							Required:    true,
+							Description: "A 6 character hex code, without the leading '#', identifying the color of the label.",
+						},
+						"description": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "A short description of the label.",
+						},
+						"url": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The URL to the issue label.",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func resourceGithubIssueLabelsRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Owner).v3client
+
+	owner := meta.(*Owner).name
+	repoName := d.Id()
+	ctx := context.WithValue(context.Background(), ctxId, d.Id())
+
+	options := &github.ListOptions{
+		PerPage: 100,
+	}
+
+	labels := []interface{}{}
+
+	for {
+		ls, resp, err := client.Issues.ListLabels(ctx, owner, repoName, options)
+		if err != nil {
+			return err
+		}
+		for _, l := range ls {
+			labels = append(labels, map[string]interface{}{
+				"name":        l.GetName(),
+				"color":       l.GetColor(),
+				"description": l.GetDescription(),
+				"url":         l.GetURL(),
+			})
+		}
+
+		if resp.NextPage == 0 {
+			break
+		}
+		options.Page = resp.NextPage
+	}
+
+	err := d.Set("label", labels)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func resourceGithubIssueLabelsDelete(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Owner).v3client
+
+	owner := meta.(*Owner).name
+	repoName := d.Get("repository").(string)
+	ctx := context.WithValue(context.Background(), ctxId, d.Id())
+
+	labels := d.Get("label").(*schema.Set)
+
+	for _, label := range labels.List() {
+		l := label.(map[string]interface{})
+
+		name := l["name"].(string)
+
+		_, err := client.Issues.DeleteLabel(ctx, owner, repoName, name)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func resourceGithubIssueLabelsUpdate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Owner).v3client
+
+	owner := meta.(*Owner).name
+	repoName := d.Get("repository").(string)
+	ctx := context.WithValue(context.Background(), ctxId, d.Id())
+
+	o, n := d.GetChange("label")
+	oMap := map[string]map[string]interface{}{}
+	nMap := map[string]map[string]interface{}{}
+	for _, raw := range o.(*schema.Set).List() {
+		m := raw.(map[string]interface{})
+		name := m["name"].(string)
+		oMap[name] = m
+	}
+	for _, raw := range n.(*schema.Set).List() {
+		m := raw.(map[string]interface{})
+		name := m["name"].(string)
+		nMap[name] = m
+	}
+
+	// delete
+	for name, _ := range oMap {
+		if _, ok := nMap[name]; !ok {
+			_, err := client.Issues.DeleteLabel(ctx, owner, repoName, name)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// create
+	for name, m := range nMap {
+		if _, ok := oMap[name]; !ok {
+			_, _, err := client.Issues.CreateLabel(ctx, owner, repoName, &github.Label{
+				Name:        github.String(name),
+				Color:       github.String(m["color"].(string)),
+				Description: github.String(m["description"].(string)),
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	// update
+	for name, m := range nMap {
+		if _, ok := oMap[name]; ok {
+			_, _, err := client.Issues.EditLabel(ctx, owner, repoName, name, &github.Label{
+				Name:        github.String(name),
+				Color:       github.String(m["color"].(string)),
+				Description: github.String(m["description"].(string)),
+			})
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	d.SetId(repoName)
+
+	return resourceGithubIssueLabelsRead(d, meta)
+}
+
+func resourceGithubIssueLabelsCreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*Owner).v3client
+
+	owner := meta.(*Owner).name
+	repoName := d.Get("repository").(string)
+	ctx := context.WithValue(context.Background(), ctxId, d.Id())
+
+	labels := d.Get("label").(*schema.Set)
+	for _, label := range labels.List() {
+		l := label.(map[string]interface{})
+		_, _, err := client.Issues.CreateLabel(ctx, owner, repoName, &github.Label{
+			Name:        github.String(l["name"].(string)),
+			Color:       github.String(l["color"].(string)),
+			Description: github.String(l["description"].(string)),
+		})
+		if err != nil {
+			return err
+		}
+	}
+
+	d.SetId(repoName)
+
+	return resourceGithubIssueLabelsRead(d, meta)
+}
---
+++ github/resource_github_issue_labels_test.go
@@ -0,0 +1,115 @@
+package github
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccGithubIssueLabels(t *testing.T) {
+	t.Run("authoritatively overtakes existing labels", func(t *testing.T) {
+		randomID := acctest.RandStringFromCharSet(5, acctest.CharSetAlphaNum)
+		empty := []map[string]interface{}{}
+
+		testCase := func(t *testing.T, mode string) {
+			resource.Test(t, resource.TestCase{
+				PreCheck:  func() { skipUnlessMode(t, mode) },
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					// 1. Check if some labels already exist (indicated by non-empty plan)
+					{
+						Config:             testAccGithubIssueLabelsConfig(randomID, empty),
+						ExpectNonEmptyPlan: true,
+					},
+					// 2. Check if all the labels are destroyed when the resource is added
+					{
+						Config: testAccGithubIssueLabelsConfig(randomID, empty),
+						Check:  resource.TestCheckResourceAttr("github_issue_labels.test", "label.#", "0"),
+					},
+					// 3. Check if a label can be created
+					{
+						Config: testAccGithubIssueLabelsConfig(randomID, append(empty, map[string]interface{}{
+							"name":        "foo",
+							"color":       "000000",
+							"description": "foo",
+						})),
+						Check: resource.TestCheckResourceAttr("github_issue_labels.test", "label.#", "1"),
+					},
+					// 4. Check if a label can be recreated
+					{
+						Config: testAccGithubIssueLabelsConfig(randomID, append(empty, map[string]interface{}{
+							"name":        "Foo",
+							"color":       "000000",
+							"description": "foo",
+						})),
+						Check: resource.TestCheckResourceAttr("github_issue_labels.test", "label.#", "1"),
+					},
+					// 5. Check if multiple labels can be created
+					{
+						Config: testAccGithubIssueLabelsConfig(randomID, append(empty,
+							map[string]interface{}{
+								"name":        "Foo",
+								"color":       "000000",
+								"description": "foo",
+							},
+							map[string]interface{}{
+								"name":        "bar",
+								"color":       "000000",
+								"description": "bar",
+							}, map[string]interface{}{
+								"name":        "baz",
+								"color":       "000000",
+								"description": "baz",
+							})),
+						Check: resource.TestCheckResourceAttr("github_issue_labels.test", "label.#", "3"),
+					},
+					// 6. Check if labels can be destroyed
+					{
+						Config: testAccGithubIssueLabelsConfig(randomID, empty),
+						Check:  resource.TestCheckResourceAttr("github_issue_labels.test", "label.#", "0"),
+					},
+				},
+			})
+		}
+
+		t.Run("with an anonymous account", func(t *testing.T) {
+			t.Skip("anonymous account not supported for this operation")
+		})
+
+		t.Run("with an individual account", func(t *testing.T) {
+			testCase(t, individual)
+		})
+
+		t.Run("with an organization account", func(t *testing.T) {
+			testCase(t, organization)
+		})
+	})
+}
+
+func testAccGithubIssueLabelsConfig(randomId string, labels []map[string]interface{}) string {
+	dynamic := ""
+	for _, label := range labels {
+		dynamic += fmt.Sprintf(`
+			label {
+				name = "%s"
+				color = "%s"
+				description = "%s"
+			}
+		`, label["name"], label["color"], label["description"])
+	}
+
+	return fmt.Sprintf(`
+		resource "github_repository" "test" {
+			name = "tf-acc-test-%s"
+			auto_init = true
+		}
+
+		resource "github_issue_labels" "test" {
+			repository = github_repository.test.id
+
+			%s
+		}
+	`, randomId, dynamic)
+}
